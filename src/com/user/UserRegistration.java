bbbb90e8c0bee66a19009f05b469a790938b7683
arsath ali <arsatharsha17@gmail.com>
2026-02-03 16:02:19 +0530

    Add email samples to be cleared by my regex

--- src/com/user/UserRegistration.java
+++ src/com/user/UserRegistration.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     static boolean validate(String input , String regex){
-        return Pattern.matches(input,regex);
+        return Pattern.matches(regex,input);
     }
 
     public static void main(String[] args) {
@@ -15,7 +15,7 @@ public class UserRegistration {
         System.out.print("Enter First Name: ");
         String firstName = sc.nextLine();
         String firstNameRegex = "^[A-Z][a-z]{2,}$";
-
+        
         System.out.println(
             validate(firstName,firstNameRegex)
                 ?"Valid First Name"
@@ -35,14 +35,39 @@ public class UserRegistration {
 
         //e-mail check
         System.out.print("Enter Email: ");
-        String email = sc.nextLine();
-        String emailRegex = "^[a-z]+(\\.[a-z]+)?@[a-z]+\\.co(\\.[a-z]+)?$";
 
-        System.out.println(
-            validate(email,emailRegex)
-                ?"Valid Email Id"
-                :"Invalid Email Id"
-        );
+        String [] emailSamples = {
+            "abc@yahoo.com",
+            "abc-100@yahoo.com",
+            "abc.100@yahoo.com",
+            "abc111@abc.com",
+            "abc-100@abc.net",
+            "abc.100@abc.com.au",
+            "abc@1.com",
+            "abc@gmail.com.com",
+            "abc+100@gmail.com",
+             "abc",                     // must contain '@'
+            "abc@.com.my",              // tld cannot start with dot
+            "abc123@gmail.a",           // last tld must have at least 2 characters
+            "abc123@.com",              // tld cannot start with dot
+            "abc123@.com.com",          // tld cannot start with dot
+            ".abc@abc.com",             // email cannot start with dot
+            "abc()*@gmail.com",         // only allowed characters
+            "abc@%*.com",               // only allowed characters
+            "abc..2002@gmail.com",      // double dots not allowed
+            "abc.@gmail.com",           // last character cannot be dot
+            "abc@abc@gmail.com",        // double '@' not allowed
+            "abc@gmail.com.1a",         // tld with two characters cannot contain digit
+            "abc@gmail.com.aa.au"       // multiple email tld not allowed
+        };
+        
+        String emailRegex = "^(?!.*\\.\\.)(?!\\.)(?!.*\\.$)[a-zA-Z][a-zA-Z0-9+_-]*@[a-zA-Z0-9]+(\\.[a-zA-Z]{2,})+$";
+
+        System.out.println("\nEmail Sample Validation (UC9):");
+        for (String sample : emailSamples) {
+            System.out.println(sample + " -> " +
+                    (validate(sample, emailRegex) ? "Valid" : "Invalid"));
+        }
 
         //mobile Number validation
         System.out.print("Enter Mobile Number (CC XXXXXXXXXX): ");
